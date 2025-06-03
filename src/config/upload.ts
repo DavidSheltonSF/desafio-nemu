79628d8c2cdbe954b265f1d856dfe73cc467a8e3
DavidSheltonSF <david.faria012@gmail.com>
2025-06-03 19:01:24 -0300

    feat: add multer configuration for file uploads

---
+++ src/config/upload.ts
@@ -0,0 +1,18 @@
+import multer from "multer";
+import UploadManager from "./upload-helper";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, `src/public`);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `data.xlsx`);
+  }
+})
+
+export const configUpload = () =>{
+  UploadManager.upload = multer({ storage }).single("file");
+  console.log("Upload configured")
+}
+
+
