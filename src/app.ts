24bd143ba141ccc6c91d2cd24af35af31cdbca8b
DavidSheltonSF <david.faria012@gmail.com>
2025-06-03 19:01:05 -0300

    feat: implement file upload functionality and configure routes

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,18 @@
-import express, {NextFunction, Request, Response} from "express";
-import multer from "multer"
-import { ExcelHandler } from "./helpers/ExcelHandler";
-import { JourneyService } from "./services/JourneyService";
+import express from "express";
+import { configUpload } from "./config/upload";
+import journeyRoutes from "./routes/journey"
+
+
+configUpload();
 
 const app = express();
+
 app.use(express.json());
 
 const port = 3000;
 
-app.post('/app', (req: Request, res: Response) => {
+app.use(journeyRoutes);
 
-  const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, `${__dirname}/public`);
-    },
-    filename: (req, file, cb) => {
-      cb(null, `data.xlsx`);
-    }
-  });
-
-  const upload = multer({ storage }).single("file");
-
-  upload(req, res, (err: any) => {
-    if (err instanceof multer.MulterError ){
-      res.status(500).send(err)
-    }
-
-    const filename = req.file?.filename;
-
-    console.log(filename)
-
-    res.status(200).send({
-      message: `The file ${filename} was uploaded successfuly`
-    })
-  });
-});
-
-app.get("/app/data", async (req: Request, res: Response) => {
-  const excelHandler = new ExcelHandler("src/public/data.xlsx");
-  const result = await excelHandler.readLines();
-  res.status(200).send(result);
-})
-
-app.get("/app/journeys/", async (req: Request, res: Response) => {
-  const service = new JourneyService();
-  const result = await service.execute();
-  console.log(result);
-  res.status(200).send(result[1])
-})
 
 app.listen(port, () => {
   console.log(`Server is listenign on port ${port}`);
