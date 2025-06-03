9e3028d8ca62d5dfa55241286ffd1e052eb10d3f
DavidSheltonSF <david.faria012@gmail.com>
2025-06-03 20:27:18 -0300

    feat: add GET route for reading journeys

--- src/routes/journey.ts
+++ src/routes/journey.ts
@@ -5,5 +5,6 @@ const router = Router();
 const controller = new JourneyController();
 
 router.post("/journeys", controller.upload)
+router.get("/journeys", controller.read);
 
 export default router
