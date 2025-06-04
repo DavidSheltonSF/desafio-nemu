8b0063d882412b4c03a1b1c45990e42084de5d04
DavidSheltonSF <david.faria012@gmail.com>
2025-06-04 17:27:10 -0300

    refactor: enhance documentation for orderTouchPointsByDate function and set a type for its return

--- backend/src/services/helpers/orderTouchPointsByDate.ts
+++ backend/src/services/helpers/orderTouchPointsByDate.ts
@@ -1,8 +1,16 @@
 import { TouchPoint } from "../../models/TouchPoint"
 
-export function oderTouchPointsByDate(list: Array<Omit<TouchPoint, "sessionId">>) {
 
-    const tempList = list.slice();
+/**
+ * 
+ * Order an array of touch points by their creation date
+ * 
+ * @param touchPoints - An array of touch points
+ * @returns An array of touch points ordered by their creation date
+ */
+export function oderTouchPointsByDate(touchPoints: Array<Omit<TouchPoint, "sessionId">>): Array<Omit<TouchPoint, "sessionId">> {
+
+    const tempList = touchPoints.slice();
 
     tempList.sort((a, b) => {
       return Date.parse(a.createdAt) - Date.parse(b.createdAt);
