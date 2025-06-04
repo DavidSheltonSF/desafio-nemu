2032721f23a85bf27f3e858185c1dfae8df87f44
DavidSheltonSF <david.faria012@gmail.com>
2025-06-04 17:28:10 -0300

    refactor: correct  documentation for removeDubplicatedTouchPoints function

--- backend/src/services/helpers/removeDuplicatedTouchPoints.ts
+++ backend/src/services/helpers/removeDuplicatedTouchPoints.ts
@@ -7,7 +7,7 @@ import { TouchPoint } from "../../models/TouchPoint";
  * 
  * @param touchPoints - An array of touch points
  * @param canRepeat - An array containing the indices of the touch points that will be returned even if they have duplicated sources
- * @returns an array without touch points  with duplicated sources
+ * @returns An array without touch points  with duplicated sources
  */
 export function removeDubplicatedTouchPoints(touchPoints: Omit<TouchPoint, "sessionId">[], canRepeat: number[]): Omit<TouchPoint, "sessionId">[]{
 
