38551eb3aa3e8032365806e84d9ad337c8267592
DavidSheltonSF <david.faria012@gmail.com>
2025-06-04 17:22:40 -0300

    refactor: enhance documentation for removeDubplicatedTouchPoints function and set a type for its return

--- backend/src/services/helpers/removeDuplicatedTouchPoints.ts
+++ backend/src/services/helpers/removeDuplicatedTouchPoints.ts
@@ -1,6 +1,15 @@
 import { TouchPoint } from "../../models/TouchPoint";
 
-export function removeDubplicatedTouchPoints(touchPoints: Omit<TouchPoint, "sessionId">[], canRepeat: number[]){
+
+/**
+ * 
+ * Removes touch points with duplicated sources
+ * 
+ * @param touchPoints - An array of touch points
+ * @param canRepeat - An array containing the indices of the touch points that will be returned even if they have duplicated sources
+ * @returns an array without touch points  with duplicated sources
+ */
+export function removeDubplicatedTouchPoints(touchPoints: Omit<TouchPoint, "sessionId">[], canRepeat: number[]): Omit<TouchPoint, "sessionId">[]{
 
   const result: Omit<TouchPoint, "sessionId">[] = []
   const registeredSources: string[] = [];
