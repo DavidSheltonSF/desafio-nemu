4beed11d70c754725b1b42c8dd478e2c4ec3d63d
DavidSheltonSF <david.faria012@gmail.com>
2025-06-04 13:16:18 -0300

    fix: handle undefined filePath in ExcelHandler constructor

--- backend/src/services/helpers/ExcelHandler.ts
+++ backend/src/services/helpers/ExcelHandler.ts
@@ -2,14 +2,18 @@ import readXlsxFile from "read-excel-file/node";
 
 
 export class ExcelHandler {
-  private readonly filePath: string;
+  private readonly filePath: string | undefined;
 
-  constructor(filePath: string){
+  constructor(filePath: string | undefined){
     this.filePath = filePath;
   }
 
   async readLines(): Promise<Array<any>> {
     const result: Array<any> = [];
+
+    if(!this.filePath){
+      throw new Error("File path is undefined");
+    }
 
     await readXlsxFile(this.filePath).then(row => result.push(row));
 
