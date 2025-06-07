564d01ea050d7202bed1adb92ce1cac2fa001b79
DavidSheltonSF <david.faria012@gmail.com>
2025-06-06 22:03:31 -0300

    style(models): fix formatting with prettier

--- backend/src/models/Journey.ts
+++ backend/src/models/Journey.ts
@@ -1,7 +1,7 @@
-import { TouchPoint } from "./TouchPoint";
+import { TouchPoint } from './TouchPoint';
 
 export interface Journey {
-  id?: string,
-  sessionId: string,
-  touchPoints: Omit<TouchPoint, "sessionId">[]
+  id?: string;
+  sessionId: string;
+  touchPoints: Omit<TouchPoint, 'sessionId'>[];
 }
--- backend/src/models/TouchPoint.ts
+++ backend/src/models/TouchPoint.ts
@@ -1,10 +1,11 @@
 export interface TouchPoint {
-  id?: string,
-  sessionId: string
-  createdAt: string,
-  source: string
+  id?: string;
+  sessionId: string;
+  createdAt: string;
+  source: string;
 }
 
-export interface TouchPointWithOptionalSessionId extends Omit<TouchPoint, "sessionId"> {
-  sessionId?: string
+export interface TouchPointWithOptionalSessionId
+  extends Omit<TouchPoint, 'sessionId'> {
+  sessionId?: string;
 }
