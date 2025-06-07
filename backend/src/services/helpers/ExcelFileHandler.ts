03a9a987e00b47147f191350b379de81827768da
DavidSheltonSF <david.faria012@gmail.com>
2025-06-06 22:00:49 -0300

    style(ExcelFileHandler): fix formatting with prettier

--- backend/src/services/helpers/ExcelFileHandler.ts
+++ backend/src/services/helpers/ExcelFileHandler.ts
@@ -1,21 +1,20 @@
-import readXlsxFile from "read-excel-file/node";
-
+import readXlsxFile from 'read-excel-file/node';
 
 export class ExcelFileHandler {
   private readonly filePath: string | undefined;
 
-  constructor(filePath: string | undefined){
+  constructor(filePath: string | undefined) {
     this.filePath = filePath;
   }
 
   async readLines(): Promise<Array<any>> {
     const data: Array<any> = [];
 
-    if(!this.filePath){
-      throw new Error("File path is undefined");
+    if (!this.filePath) {
+      throw new Error('File path is undefined');
     }
 
-    await readXlsxFile(this.filePath).then(row => data.push(row));
+    await readXlsxFile(this.filePath).then((row) => data.push(row));
 
     return data;
   }
