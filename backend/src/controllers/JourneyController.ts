61c483c5039abba4ca1c6a4521e1a7e8887c4b9d
DavidSheltonSF <david.faria012@gmail.com>
2025-06-06 22:06:33 -0300

    style(controllers): fix formatting with prettier

--- backend/src/controllers/JourneyController.ts
+++ backend/src/controllers/JourneyController.ts
@@ -1,61 +1,52 @@
-import { Request, Response } from "express";
-import { JourneyService } from "../services/JourneyService";
-import UploadManager from "../config/upload-helper";
-import multer from "multer";
+import { Request, Response } from 'express';
+import { JourneyService } from '../services/JourneyService';
+import UploadManager from '../config/upload-helper';
+import multer from 'multer';
 
 const service = new JourneyService();
 
 export class JourneyController {
-  async upload(req: Request, res: Response){
-
+  async upload(req: Request, res: Response) {
     try {
-
-      if(UploadManager.upload){
-
+      if (UploadManager.upload) {
         UploadManager.upload(req, res, (err: any) => {
-
-          if (err instanceof multer.MulterError ){
+          if (err instanceof multer.MulterError) {
             console.log(err);
             res.status(500).send({
-              message: "Internal Error"
+              message: 'Internal Error',
             });
           }
 
           const filename = req.file?.filename;
-      
+
           res.status(200).send({
-            message: `The file ${filename} was uploaded successfuly`
+            message: `The file ${filename} was uploaded successfuly`,
           });
         });
-
-      }
-
-      else {
-        console.log("Upload was not configured");
+      } else {
+        console.log('Upload was not configured');
         res.status(500).send({
-          message: "Internal Error"
+          message: 'Internal Error',
         });
       }
-      
     } catch (error) {
       console.log(error);
       res.status(500).send({
-        message: "Internal Error"
+        message: 'Internal Error',
       });
     }
   }
 
-  async read(req: Request, res: Response){
+  async read(req: Request, res: Response) {
     try {
       const result = await service.execute();
       res.status(200).send({
-        data: result
-      })
-      
+        data: result,
+      });
     } catch (error) {
       console.log(error);
       res.status(500).send({
-        message: "Internal Error"
+        message: 'Internal Error',
       });
     }
   }
