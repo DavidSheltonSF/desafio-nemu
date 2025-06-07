907f17debd2c6b80052f8c207965ba8c22124458
DavidSheltonSF <david.faria012@gmail.com>
2025-06-06 21:58:43 -0300

    style(removeDuplicatedTouchPoints): fix formatting with prettier

--- backend/src/services/helpers/removeDuplicatedTouchPoints.ts
+++ backend/src/services/helpers/removeDuplicatedTouchPoints.ts
@@ -1,22 +1,24 @@
-import { TouchPointWithOptionalSessionId } from "../../models/TouchPoint";
+import { TouchPointWithOptionalSessionId } from '../../models/TouchPoint';
 
-export function removeDubplicatedTouchPoints(touchPoints: TouchPointWithOptionalSessionId[], repeatableIndexes: number[]): TouchPointWithOptionalSessionId[]{
-  const filteredTouchPoints: TouchPointWithOptionalSessionId[] = []
+export function removeDubplicatedTouchPoints(
+  touchPoints: TouchPointWithOptionalSessionId[],
+  repeatableIndexes: number[]
+): TouchPointWithOptionalSessionId[] {
+  const filteredTouchPoints: TouchPointWithOptionalSessionId[] = [];
   const registeredSources: string[] = [];
 
-  for (let i=0; i<touchPoints.length; i++){
+  for (let i = 0; i < touchPoints.length; i++) {
     const currentTouchPoint = touchPoints[i];
 
-    if(repeatableIndexes.includes(i)){
+    if (repeatableIndexes.includes(i)) {
       filteredTouchPoints.push(currentTouchPoint);
       continue;
     }
-    
-    if(!registeredSources.includes(currentTouchPoint.source)){
-      filteredTouchPoints.push(currentTouchPoint);
-      registeredSources.push(currentTouchPoint.source); 
-    }
 
+    if (!registeredSources.includes(currentTouchPoint.source)) {
+      filteredTouchPoints.push(currentTouchPoint);
+      registeredSources.push(currentTouchPoint.source);
+    }
   }
 
   return filteredTouchPoints;
