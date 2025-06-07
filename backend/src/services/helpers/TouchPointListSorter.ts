277a9a436c0b8493eaaa8454288d7c4f5f24719c
DavidSheltonSF <david.faria012@gmail.com>
2025-06-06 21:48:35 -0300

    style(TouchPointListSorter): simplify the return format of .sort() method

--- backend/src/services/helpers/TouchPointListSorter.ts
+++ backend/src/services/helpers/TouchPointListSorter.ts
@@ -6,19 +6,20 @@ export class TouchPointListSorter {
   static sortByNewest(touchPoints: TouchPointWithOptionalSessionId[]): TouchPointWithOptionalSessionId[]{
     const sortedTouchPoints = touchPoints.slice();
 
-    sortedTouchPoints.sort((a, b) => {
-      return Date.parse(a.createdAt) - Date.parse(b.createdAt);
-    });
+    sortedTouchPoints.sort(
+      (a, b) => Date.parse(a.createdAt) - Date.parse(b.createdAt)
+    );
 
     return sortedTouchPoints
   }
 
   static sortByOldest(touchPoints: TouchPointWithOptionalSessionId[]): TouchPointWithOptionalSessionId[] {
+
     const sortedTouchPoints = touchPoints.slice();
 
-    sortedTouchPoints.sort((a, b) => {
-      return Date.parse(b.createdAt) - Date.parse(a.createdAt);
-    });
+    sortedTouchPoints.sort(
+      (a, b) => Date.parse(b.createdAt) - Date.parse(a.createdAt)
+    );
 
     return sortedTouchPoints
   }
